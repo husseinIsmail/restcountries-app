ebe5c95deb6536755934d4044e150657452e0daf
husseinismail <hismail.dev@gmail.com>
2023-01-07 17:37:48 +0000

    Create React TypeScript app

---
+++ world-countries/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = (): JSX.Element => {
+  return (
+    <div className="app">
+    </div>
+  );
+}
+
+export default App;
