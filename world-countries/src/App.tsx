f3d3fbc6ac8f20260a8b290a4b22a155c2ea2f83
husseinismail <hismail.dev@gmail.com>
2023-01-07 19:37:52 +0000

    Add header

--- world-countries/src/App.tsx
+++ world-countries/src/App.tsx
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const App = (): JSX.Element => {
   return (
     <div className="app">
+      <header className="app-header">
+        <span className="app-header-title">Where in the world?</span>
+      </header>
     </div>
   );
 }
