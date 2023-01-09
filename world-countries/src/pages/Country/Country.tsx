864c56a232cc501cc42d964fef022e73ab7264e5
husseinismail <hismail.dev@gmail.com>
2023-01-09 13:10:27 +0000

    Add country page back btn

--- world-countries/src/pages/Country/Country.tsx
+++ world-countries/src/pages/Country/Country.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { CountryExtendedInfo } from "../../interfaces/CountryExtendedInfo";
 import { useParams } from "react-router-dom";
 import './Country.css';
+import { useNavigate } from 'react-router-dom';
 
 const Country = (): JSX.Element => {
   const [countryInfo, setCountryInfo] = useState<CountryExtendedInfo | null>(null);
   const { countryName } = useParams();
   const [apiError, setApiError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`https://restcountries.com/v2/name/${countryName}?fulltext=true,fields=name,nativeName,region,subregion,capital,topLevelDomain,currencies,languages,borders,flag`)
@@ -27,7 +29,13 @@ const Country = (): JSX.Element => {
 
   if (apiError) return <p>{apiError}</p>
   else return (
-    <p>{JSON.stringify(countryInfo)}</p>
+    <Container>
+      <Row>
+        <Col sm={2} xs={4}>
+          <Button onClick={() => navigate('/')} className='back-btn'><i className="fa-solid fa-arrow-left back-btn-icon"></i>Back</Button>
+        </Col>
+      </Row>
+    </Container>
   )
 };
 
