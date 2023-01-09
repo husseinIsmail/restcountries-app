69bc479949f990c99a1265d42a18618041062c44
husseinismail <hismail.dev@gmail.com>
2023-01-09 20:37:05 +0000

    Fix Home.test after adding loader

--- world-countries/src/pages/Home/Home.test.tsx
+++ world-countries/src/pages/Home/Home.test.tsx
@@ -24,7 +24,7 @@ describe('Home page', () => {
   it('Shows filtered countries based on search', async () => {
     render(<BrowserRouter><Home /></BrowserRouter>);
     const user = userEvent.setup();
-    await user.type(screen.getByPlaceholderText('Search for a country...'), 'B');
+    await user.type(await screen.findByPlaceholderText('Search for a country...'), 'B');
 
     await waitFor(() => {
       expect(screen.getAllByText('Population:')).toHaveLength(1);
@@ -38,8 +38,8 @@ describe('Home page', () => {
   it('Shows filtered countries based on region', async () => {
     render(<BrowserRouter><Home /></BrowserRouter>);
     const user = userEvent.setup();
-    await user.click(screen.getByText('Filter by Region'));
-    await user.click(screen.getByText('Americas', { selector: 'a' }));
+    await user.click(await screen.findByText('Filter by Region'));
+    await user.click(await screen.findByText('Americas', { selector: 'a' }));
 
     await waitFor(async () => {
       expect(screen.getAllByText('Population:')).toHaveLength(1);
@@ -53,7 +53,7 @@ describe('Home page', () => {
   it('Sorts the country cards based on population', async () => {
     render(<BrowserRouter><Home /></BrowserRouter>);
     const user = userEvent.setup();
-    await user.click(screen.getByText('Sort by Population'));
+    await user.click(await screen.findByText('Sort by Population'));
 
     await waitFor(() => {
       expect(screen.getAllByText('Population:')).toHaveLength(fakeCountries.length);
