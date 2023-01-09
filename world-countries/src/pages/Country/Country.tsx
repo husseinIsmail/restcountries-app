3b5d6e114cbcc06d6867a5e19c3b711486691e43
husseinismail <hismail.dev@gmail.com>
2023-01-09 13:07:33 +0000

    Refactor fetchCountryInfo

--- world-countries/src/pages/Country/Country.tsx
+++ world-countries/src/pages/Country/Country.tsx
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react";
 import { CountryExtendedInfo } from "../../interfaces/CountryExtendedInfo";
 import { useParams } from "react-router-dom";
+import './Country.css';
 
 const Country = (): JSX.Element => {
-
-  const [countryInfo, setCountryInfo] = useState<CountryExtendedInfo | {}>({});
+  const [countryInfo, setCountryInfo] = useState<CountryExtendedInfo | null>(null);
   const { countryName } = useParams();
+  const [apiError, setApiError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchCountryInfo = async () => {
-      const fetchedCountryInfo = await fetch(`https://restcountries.com/v2/name/${countryName}?fields=name,nativeName,region,subregion,capital,topLevelDomain,currencies,languages,borders`);
-      setCountryInfo(await fetchedCountryInfo.json());
-    };
-
-    fetchCountryInfo();
+    fetch(`https://restcountries.com/v2/name/${countryName}?fulltext=true,fields=name,nativeName,region,subregion,capital,topLevelDomain,currencies,languages,borders,flag`)
+      .then(res => res.json())
+      .then(
+        (fetchedCountryInfo) => {
+          if (fetchedCountryInfo[0].name) {
+            setCountryInfo(fetchedCountryInfo[0]);
+          } else {
+            console.error('Error fetching country info from restcountries API: ', fetchedCountryInfo);
+            setApiError('*Error fetching country info, please double check the name in the URL*');
+          }
+        },
+        (error) => {
+          setApiError(error);
+        });
   }, [countryName]);
 
-  return (
-    <h1>{JSON.stringify(countryInfo)}</h1>
-  );
+  if (apiError) return <p>{apiError}</p>
+  else return (
+    <p>{JSON.stringify(countryInfo)}</p>
+  )
 };
 
 export default Country;
