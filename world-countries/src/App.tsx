e8f461ded1ced1050b63127d92bdeb6e23e90376
husseinismail <hismail.dev@gmail.com>
2023-01-09 10:58:20 +0000

    Add routing

--- world-countries/src/App.tsx
+++ world-countries/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const App = (): JSX.Element => {
   return (
@@ -8,7 +9,12 @@ const App = (): JSX.Element => {
       <header className="app-header">
         <span className="app-header-title">Where in the world?</span>
       </header>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
